997d0f1cbfe3ed1362e22477dbc1617e4211db2f
Marharyta Ponomarenko <margo.marharet@gmail.com>
2024-12-27 01:10:34 +0100

    feat: added game saving and loading with quest data support

--- RPG/Assets/Scripts/GameMenu.cs
+++ RPG/Assets/Scripts/GameMenu.cs
@@ -213,4 +213,10 @@ public class GameMenu : MonoBehaviour
         activeItem.Use(selectChar);
         CloseItemCharChoice();
     }
+
+    public void SaveGame()
+    {
+        GameManager.instance.SaveData();
+        QuestManager.instance.SaveQuestData();
+    }
 }

062edef4f6d953a5e0d9cb410b8866f11b52d904
Marharyta Ponomarenko <margo.marharet@gmail.com>
2024-12-28 12:20:42 +0100

    feat: created layout for battle scene & battle character setup

---
+++ RPG/Assets/Scripts/BattleChar.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+public class BattleChar : MonoBehaviour
+{
+    public bool isPlayer;
+    public string[] movesAvailable;
+
+    public string charName;
+    public int currentHp, maxHP, currentMP, maxMP, strength, defence, wpnPower, armrPower;
+    public bool hasDied;
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
